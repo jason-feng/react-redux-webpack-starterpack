4dc7192dbed42ed3d0b8edf7ef27228874f938bb
Jason Feng <jason21feng@gmail.com>
2017-04-25 21:56:33 -0400

    add connected component counter

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Counter from '../containers/counter';
 
 const Welcome = (props) => {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      Welcome
+      <Counter />
+    </div>
+  );
 };
 
 export default Welcome;
